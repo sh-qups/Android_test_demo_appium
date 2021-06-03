c76e95476c4f247e15fe7c4390acecc51b040627
shashwata <shashwatahalder01@gmail.com>
2021-06-03 13:05:27 +0600

    commited by sh Jun_03 added desird_cap

--- TestCase/base_test.py
+++ TestCase/base_test.py
@@ -15,6 +15,17 @@ class BaseTest(unittest.TestCase):
                                   'app': '/home/rk/PycharmProjects/Android_test_demo_appium/Apk/Bikroy Sell Rent Buy Find Jobs_v1.2.01_apkpure.com.apk'
                                   })
 
+        # desired_capabilities = {'platformName': 'Android',
+        #                         # 'automationName': 'UiAutomator2',
+        #                         'deviceName': 'emulator-5554',
+        #                         # 'platformVersion': '11.0.0',
+        #                         'app-package': 'com.bikroy',
+        #                         'app-activity': 'se.saltside.activity.main.MainActivity',
+        #                         # 'app': '/home/asif-rouf/software/Bikroy Sell Rent Buy Find Jobs_v1.2.01_apkpure.com.apk'
+        #                         'app': '/home/rk/PycharmProjects/Android_test_demo_appium/Apk/Bikroy Sell Rent Buy Find Jobs_v1.2.01_apkpure.com.apk'
+        #                         }
+        # self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723/wd/hub',desired_capabilities=desired_capabilities)
+
     def tearDown(self):
         # self.driver.close()
         self.driver.quit()
